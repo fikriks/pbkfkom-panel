114306d61a135426bdaf5ae5ed7fdd915dd11df3
fikriks <fikriks130@gmail.com>
2023-08-27 01:17:53 +0700

    update: home controller :rocket:

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,33 @@
 
 namespace App\Controllers;
 
+use App\Models\StudentModel;
+
 class Home extends BaseController
 {
+    private $Student;
+
+    public function __construct()
+    {
+        $this->Student  = new StudentModel();
+    }
+
     public function index(): string
     {
+
         return view('welcome_message');
+    }
+
+    public function search()
+    {
+        $name = $this->request->getPost('name');
+
+        $students = $this->Student->like('nama', $name)->findAll();
+
+        $data = [
+            'students' => $students
+        ];
+
+        return view('students', $data);
     }
 }
